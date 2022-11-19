c613b699e684b2fade3543a5780476402953d819
Akira Nguyen <akiranguyen@Akiras-MacBook-Pro.local>
2022-11-19 21:33:52 +0700

    minimalize

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,8 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
-import Icon from '@mui/material/Icon';
-import DeleteIcon from '@mui/icons-material/Delete';
-import Grid from '@mui/material/Grid';
-// import CountrySelect from './autocomplete'
-// import GoogleMaps from './autocomplete22'
-import Asynchronous from './autocomplete3'
+import GAutocomplete from './autocomplete'
 
 const introduction = "Please try Autocomplete below"
 export default function Home({ allPostsData }) {
@@ -25,9 +15,7 @@ export default function Home({ allPostsData }) {
 
       <section className={utilStyles.headingMd}>
         <p>{introduction} <ArrowDownwardIcon /></p>
-        {/* <CountrySelect /> */}
-        {/* <GoogleMaps /> */}
-        <Asynchronous />
+        <GAutocomplete />
       </section>
 
     </Layout>
