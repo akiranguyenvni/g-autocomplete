75098055139143c2acd62266481fb920993b601e
Akira Nguyen <akiranguyen@Akiras-MacBook-Pro.local>
2022-11-19 15:58:48 +0700

    removed: avatar

--- components/layout.js
+++ components/layout.js
@@ -6,7 +6,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = "Hi! I'm G"
+const name = "Hi! I'm G-Search"
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
@@ -37,27 +37,27 @@ export default function Layout({ children, home }) {
       <header className={styles.header}>
         {home ? (
           <>
-            <Image
+            {/* <Image
               priority
               src="/images/profile.jpg"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
               alt={name}
-            />
+            /> */}
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
           </>
         ) : (
           <>
             <Link href="/">
-              <Image
+              {/* <Image
                 priority
                 src="/images/profile.jpg"
                 className={utilStyles.borderCircle}
                 height={108}
                 width={108}
                 alt={name}
-              />
+              /> */}
             </Link>
             <h2 className={utilStyles.headingLg}>
               <Link href="/" className={utilStyles.colorInherit}>
